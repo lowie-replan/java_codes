fce4a4776118c034c7fb33a4ee7db75b7c3a24ce
Lowie John Replan <replanlowiejohn@gmail.com>
2026-04-22 23:09:03 +0800

    matrix sample

---
+++ loopings/matrixSample.java
@@ -0,0 +1,35 @@
+package loopings;
+
+import java.util.Scanner;
+
+public class matrixSample {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        // user inputs - matrix dimension
+        System.out.print("Enter number of rows: ");
+        int rows = input.nextInt();
+        System.out.print("Enter number of columns: ");
+        int cols = input.nextInt();
+
+        int[][] matrix = new int[rows][cols];
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                System.out.printf("Enter element for [%d][%d]: ", i, j);
+                matrix[i][j] = input.nextInt();
+            }
+        }
+
+        int total = 0;
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                System.out.print(matrix[i][j] + "\t");
+                total += matrix[i][j];
+            }
+            System.out.println();
+        }
+
+        System.out.println("Total sum of all elements: " + total);
+    }
+}
